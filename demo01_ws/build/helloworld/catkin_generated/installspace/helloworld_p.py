4a9fe6d9997e7fc830f358f5391558c0f8f1ebda
YhQIAO <970445893@qq.com>
2021-10-15 20:24:31 +0800

    day01

---
+++ demo01_ws/build/helloworld/catkin_generated/installspace/helloworld_p.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import rospy
+if __name__ == "__main__":
+    rospy.init_node("hello_p")
+    rospy.loginfo("hello world by python")
---
+++ demo2_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/qyh/Desktop/ROS_Tutorial/demo2_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/qyh/Desktop/ROS_Tutorial/demo2_ws/devel;/opt/ros/noetic'.split(';') if '/home/qyh/Desktop/ROS_Tutorial/demo2_ws/devel;/opt/ros/noetic' != '' else []
